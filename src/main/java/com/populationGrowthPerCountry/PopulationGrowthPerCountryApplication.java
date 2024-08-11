1d27ed2dd9c77b2204c3752e0d6b5838b606019b
Natthaphon Thongtrong <natthaphonthongtrong@Natthaphon-SRS.local>
2024-08-11 21:33:23 +0700

    [Enhanced]: print service version

--- src/main/java/com/populationGrowthPerCountry/PopulationGrowthPerCountryApplication.java
+++ src/main/java/com/populationGrowthPerCountry/PopulationGrowthPerCountryApplication.java
@@ -1,13 +1,19 @@
 package com.populationGrowthPerCountry;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PopulationGrowthPerCountryApplication {
+public class PopulationGrowthPerCountryApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PopulationGrowthPerCountryApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("========== Service V.2 is running ==========");
+		
 	}
 
 }
